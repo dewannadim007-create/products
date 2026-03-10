3e53fcfc247cb86aec7941db5f241ae2c6ffbbb8
Nadim <dewannadim007@gmail.com>
2026-03-10 18:45:31 +0600

    Prepare project for Render deployment

--- practical/settings.py
+++ practical/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-=3ut63v%8^s$!rbfomq65hip0&!frquwsm4rzkbwi06c*5$d28
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['products-jk9t.onrender.com']
 
 
 # Application definition
@@ -118,6 +118,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
